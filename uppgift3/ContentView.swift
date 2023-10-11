6976e25cbde1fda05c5e404bd12c1a5966ccc56f
Anton_Sme <anton@smedberg.eu>
2023-10-11 11:54:35 +0200

    Initial Commit

---
+++ uppgift3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  uppgift3
+//
+//  Created by Anton Smedberg on 2023-10-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ uppgift3/uppgift3App.swift
@@ -0,0 +1,17 @@
+//
+//  uppgift3App.swift
+//  uppgift3
+//
+//  Created by Anton Smedberg on 2023-10-11.
+//
+
+import SwiftUI
+
+@main
+struct uppgift3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
